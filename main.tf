0b0b6034b995799ee903e580f6793f6bdb08b486
ganesh-azgit <92446859+ganesh-azgit@users.noreply.github.com>
2022-04-21 00:32:30 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -19,9 +19,11 @@ resource "aws_instance" "ganesh-2022" {
   #!/bin/bash
   sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
   sed -i 's/UsePAM no/UsePAM yes/' /etc/ssh/sshd_config
+  sed -i 's/#PermitRootLogin yes/PermitRootLogin yes/' /etc/ssh/sshd_config
   systemctl restart sshd.service
   useradd ganesh
   echo "test@123"|passwd --stdin ganesh
+  echo "test@123"|passwd --stdin root
   echo "ganesh ALL=(ALL) NOPASSWD:ALL"|tee -a /etc/sudoers.d/90-cloud-init-users
   EOF
 }
