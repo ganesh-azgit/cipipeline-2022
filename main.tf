500686c83aeba42562026cfee70efd14083143eb
ganesh-azgit <92446859+ganesh-azgit@users.noreply.github.com>
2022-04-20 21:54:33 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,5 +7,7 @@ resource "aws_instance" "ganesh-2022" {
   count         = 2
   ami           = var.image_id
   instance_type = var.instance_type
-  tags          = var.tags.${count.index}
+  tags          = {
+    Name = "ganesh-2022-${count.index}"
+  }
 }
